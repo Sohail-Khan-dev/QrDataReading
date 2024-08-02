793912f1c48155178ee86e588fff67754d61b436
Shinwari <sohailciit38@gmail.com>
2024-08-02 23:56:55 +0500

    schedule the time and set the time zone to Riayad

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,11 +12,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
-        $schedule->command('backup:run')->dailyAt('00:00');
-        $schedule->command('backup:run')->dailyAt('06:00');
-        $schedule->command('backup:run')->dailyAt('12:00');
-        $schedule->command('backup:run')->dailyAt('18:00');
+        $schedule->command('backup:run')->dailyAt('15:30');
+        $schedule->command('backup:run')->dailyAt('17:00');
+        $schedule->command('backup:run')->dailyAt('18:30');
+        $schedule->command('backup:run')->dailyAt('19:30');
         $schedule->command('backup:clean')->dailyAt('01:00');       
     }
 
